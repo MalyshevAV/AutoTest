6ff87e68be276e6e7e4632ecc89b2188a4ecd275
Malyshev Aleksandr <a.malyshev@ymail.com>
2023-08-30 16:30:08 +0300

    result 30.08

--- MDM/src/test/java/AutoTest/Nomenclature.java
+++ MDM/src/test/java/AutoTest/Nomenclature.java
@@ -1,10 +1,20 @@
 package AutoTest;
 
+import groovy.json.JsonToken;
 import io.qameta.allure.Description;
 import io.restassured.RestAssured;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -30,13 +40,49 @@ public class Nomenclature {
     @Description("НЕ ДОПИЛЕН Получение номенклатуры по Гуид, валидация по схеме Json")
     public void getNomenclatureGuid(){
         given()
-                .auth().basic("Administrator", "1234567809").when()
+                .auth().basic("Administrator", "1234567809")
+                .when()
                 .get("nomenclature/937a6068-3d9b-11ee-918f-7824af8ab721/6cec812a-35d6-11ee-918f-7824af8ab721")
                 .then().log().all()
                 .assertThat()
                 .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("getNomenclatureGuid.json"))
                 .statusCode(200);
     }
+//    @Test
+//    @Description("Создаем заявку на добавление, изменение, удаление номенклатуры")
+//    // Using HashMap
+//    public void postNomenclatureBe() throws FileNotFoundException {
+//        Map<String, Object> jsonAsMap = new HashMap<>();
+//        jsonAsMap.put("firstName", "John");
+//        jsonAsMap.put("lastName", "Doe");
+//        given()
+//                .auth().basic("Administrator", "1234567809").and()
+//                .header("x-se-hash",  "cfcd208495d565ef66e7dff9f98764da")
+//                .body(jsonAsMap)
+//                .when()
+//                .post("nomenclature/937a6068-3d9b-11ee-918f-7824af8ab721")
+//                .then().log().all()
+//                .assertThat()
+//               // .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("getBasicServicesGuid.json"))
+//                .statusCode(200);
+//   }
 
+    @Test
+    @Description("Создаем заявку на добавление, изменение, удаление номенклатуры")
+    // Using Json File
+    public void postNomenclatureBeUsingJsonFile()  {
+        File file = new File("C:\\Users\\Sasha\\TEST\\Auto_test\\MDM\\src\\test\\resources\\postNomenclatureUsingJsonFil.json");
 
+        given()
+                .auth().basic("Administrator", "1234567809").and()
+                .header("x-se-hash",  "cfcd208495d565ef66e7dff9f98764da")
+               // .multiPart(new File(".\\src\\test\\resources\\postNomenclatureUsingJsonFil.json"))
+                .body(file)
+                .when()
+                .post("/nomenclature/937a6068-3d9b-11ee-918f-7824af8ab721")
+                .then().log().all()
+                .assertThat()
+                // .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("getBasicServicesGuid.json"))
+                .statusCode(200);
+    }
 }
