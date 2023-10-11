dc4c3f3aaad6c6a2d6efc75344364e59523ff1be
Malyshev Aleksandr <a.malyshev@ymail.com>
2023-10-11 17:43:34 +0300

    listOfChange

--- MDM/src/test/java/AutoTest/Application.java
+++ MDM/src/test/java/AutoTest/Application.java
@@ -18,17 +18,30 @@ public class Application {
 
 
     @Test
-    @Description("Получение номенклатуры по Гуид, валидация по схеме Json")
+    @Description("Получение заявки по Гуид на изменение Type = 5 , валидация по схеме Json")
     public void getChangeRequestGuid() {
         installSpec(requestSpecification(), responseSpecification());
         given()
                 .when()
-                .get("change-request/b396da63-61f7-11ee-b5b0-005056013b0c")
+                .get("change-request/31c9ca2a-b120-41f8-aa86-8cffb2627492")
                 .then().log().all()
-                .assertThat()
-                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("getChangeRequest.json"));
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("getChangeRequest.json"));
         deleteSpec();
     }
+
+
+    @Test (dataProvider = "guidNegative", dataProviderClass = Nomenclature.class)
+    @Description("Негативные тесты Получение измеений по заявке по Гуид ")
+    public void getChangeRequestGuidNegative(Object guid) {
+        installSpec(requestSpecification(), responseSpecification400());
+        given()
+                .when()
+                .pathParam("guid", guid)
+                .get("change-request/{guid}")
+                .then().log().all();
+        deleteSpec();
+    }
+
 
 
 
--- MDM/src/test/java/AutoTest/Auxiliary.java
+++ MDM/src/test/java/AutoTest/Auxiliary.java
@@ -9,25 +9,58 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.Assertions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 import static Specifications.Specifications.*;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class Auxiliary {
+
+    @DataProvider
+    @Description("Позитивные тесты с использованием DataProvider")
+    public static Object[][] data() {
+        return new Object[][]{
+                {"nomenclature", 1},
+                {"basic-services", 1},
+                {"unified-classifier", 1},
+                {"eop", 1},
+                {"units", 1},
+                {"okpd2", 1},
+                {"okved2", 1},
+                {"tnved", 1}
+        };
+    }
+        @DataProvider
+        @Description("Негативные тесты с использованием DataProvider")
+        public static Object[][] dataNegative() {
+            return new Object[][]{
+                    {"", 1},
+                    {"basic-services", 201},
+                    {"unified-classifier", 0},
+                    {"eop", -1},
+                    {"units", Double.MAX_VALUE},
+                    {"okpd2", Integer.MAX_VALUE},
+                    {"okved2", "123string"},
+                    {"tnved", "<script>alert( 'Hello world' );</script>"}
+            };
+    }
+
     @Test
     @Description("Получение ОКПД2 по Гуид, валидация при помощи схемы Json")
     public void getSetNewHash() {
         installSpec(requestSpecification(), responseSpecification());
         given()
-                .header("x-se-hash",  "cfcd208495d565ef66e7dff9f98764da")
+                .header("x-se-hash", "cfcd208495d565ef66e7dff9f98764da")
                 .when()
                 .get("/set-new-hash")
                 .then().log().all();
         deleteSpec();
     }
+
     @Test
     @Description("Получение списка бизнес единиц")
     public void getBeList() {
@@ -38,22 +71,52 @@ public class Auxiliary {
                         .then().log().all().
                         extract().body().jsonPath().getList(".", getBeListPojo.class);
         response.forEach(x -> Assert.assertEquals(x.getGuid().length(), 36));
-        response.forEach(x-> Assert.assertTrue(x.getNameFull().length() <= 150));
-        response.forEach(x-> Assert.assertTrue(x.getName().length() <= 255));
-        response.forEach(x-> Assert.assertTrue(x.getInn().length() <= 12));
-        response.forEach(x-> Assert.assertEquals(x.getKpp().length(), 9));
+        response.forEach(x -> Assert.assertTrue(x.getNameFull().length() <= 150));
+        response.forEach(x -> Assert.assertTrue(x.getName().length() <= 255));
+        response.forEach(x -> Assert.assertTrue(x.getInn().length() <= 12));
+        response.forEach(x -> Assert.assertEquals(x.getKpp().length(), 9));
         Assertions.assertNotNull(response);
         deleteSpec();
     }
 
     @Test
-    @Description ("Проверка доступа и авторизации")
-    public void getPing(){
+    @Description("Проверка доступа и авторизации")
+    public void getPing() {
         installSpec(requestSpecification(), responseSpecification());
         given()
                 .when()
                 .get("/ping")
                 .then().log().all();
         deleteSpec();
+    }
+
+    @Test(dataProvider = "data")
+    @Description("Проверка списка изменений")
+    public void getListOfChanges(String type, int step) {
+        installSpec(requestSpecification(), responseSpecification());
+        given()
+                .when()
+                .pathParam("type", type)
+                .pathParam("step", step)
+                .get("list-of-changes/{type} {step}")
+                .then().log().all()
+                .assertThat()
+                .body("size()", is(step))
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("getListOfChanges.json"));
+        deleteSpec();
+
+    }
+    @Test(dataProvider = "dataNegative")
+    @Description("Проверка списка изменений")
+    public void getListOfChangesNegative(String type, Object step) {
+        installSpec(requestSpecification(), responseSpecification400());
+        given()
+                .when()
+                .pathParam("type", type)
+                .pathParam("step", step)
+                .get("list-of-changes/{type} {step}")
+                .then().log().all();
+        deleteSpec();
+
     }
 }
